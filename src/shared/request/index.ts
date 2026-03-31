f6cc28b59a2b9e21af44b22464a6c057b73dfe75
Prolip <gojimin3095@gmail.com>
2026-03-31 19:21:09 +0900

    [chore] shared request 슬라이스 공개 엔트리를 정리
    - request core 함수와 에러 타입을 shared/request index에서 일괄 export
    - 외부 레이어가 세부 파일 대신 슬라이스 엔트리만 참조하도록 정리
    - phase 2 task 문서에서 request 공개 API 항목 체크

--- src/shared/request/index.ts
+++ src/shared/request/index.ts
@@ -1,1 +1,30 @@
-export {}
+export {
+  buildRequestQueryString,
+  createRequestInit,
+  createRequestUrl,
+  handleRequestError,
+  request,
+  requestDelete,
+  requestGet,
+  requestPatch,
+  requestPost,
+  requestPut,
+} from './lib/requestCore'
+export { RequestError, RequestGetError } from './lib/requestError'
+export type {
+  CreateRequestErrorProps,
+  CreateRequestInitProps,
+  RequestBody,
+  RequestErrorHandling,
+  RequestErrorInfo,
+  RequestHeaders,
+  RequestInitWithMethod,
+  RequestJsonBody,
+  RequestMethod,
+  RequestMethodProps,
+  RequestProps,
+  RequestQueryParams,
+  RequestQueryValue,
+  RequestResponseOption,
+  WithErrorHandling,
+} from './lib/requestType'
