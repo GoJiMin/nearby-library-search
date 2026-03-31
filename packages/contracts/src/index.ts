ee845e3610a9be49a616a885cd4785eb13dae282
Prolip <gojimin3095@gmail.com>
2026-04-01 02:24:15 +0900

    [chore] contracts 패키지에 지역 코드 공통 스키마 위치를 정리
    - region과 detailRegion 공통 타입 파일 추가
    - 지역 코드 묶음용 RegionIdentifier 타입 초안 추가
    - 공개 엔트리에서 지역 관련 타입을 export하도록 연결

--- packages/contracts/src/index.ts
+++ packages/contracts/src/index.ts
@@ -1,2 +1,7 @@
 export type { Isbn, Isbn13, LibraryCode } from './identifiers'
 export type { ErrorCode, ErrorResponse } from './error'
+export type {
+  DetailRegionCode,
+  RegionCode,
+  RegionIdentifier,
+} from './region'
---
+++ packages/contracts/src/region.ts
@@ -0,0 +1,8 @@
+export type RegionCode = string
+
+export type DetailRegionCode = string
+
+export type RegionIdentifier = {
+  region: RegionCode
+  detailRegion?: DetailRegionCode
+}
