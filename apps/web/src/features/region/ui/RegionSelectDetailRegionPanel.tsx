b375ddc2c89a2abcf3a9e5a7e4efcfc8256c659e
Prolip <gojimin3095@gmail.com>
2026-04-09 22:34:55 +0900

    [style] 스타일 수정

--- apps/web/src/features/region/ui/RegionSelectDetailRegionPanel.tsx
+++ apps/web/src/features/region/ui/RegionSelectDetailRegionPanel.tsx
@@ -41,7 +41,7 @@ function RegionSelectDetailRegionPanel({
       </Heading>
       {isDetailRegionEnabled ? (
         <div className="flex min-h-0 flex-1 flex-col">
-          <ul className="flex-1 space-y-1 overflow-y-auto py-2 pr-2 [scrollbar-color:theme(colors.line)_transparent] [scrollbar-width:thin] [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-thumb]:bg-line [&::-webkit-scrollbar-track]:bg-transparent [&::-webkit-scrollbar]:w-1.5">
+          <ul className="flex-1 space-y-1 overflow-y-auto py-2 pr-2 [scrollbar-color:var(--color-line)_transparent] [scrollbar-width:thin] [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-thumb]:bg-line [&::-webkit-scrollbar-track]:bg-transparent [&::-webkit-scrollbar]:w-1.5">
             <li>
               <RegionSelectRowButton
                 isSelected={selectedDetailRegion == null}
